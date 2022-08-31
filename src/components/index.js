b4cf21b9d9ace3099b040a9d2d712083d7f1fd5c
Priyanshu Sinha <Priyanshu_Sinha@outlook.com>
2022-08-31 01:47:47 -0400

    - Minor fixes. Removed blog and projects page.
    Signed-off-by: Priyanshu Sinha <Priyanshu_Sinha@outlook.com>

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Navbar } from './Navbar/Navbar'
 export { default as Landing } from './Landing/Landing'
 export { default as About } from './About/About'
+/*
 export { default as Skills } from './Skills/Skills'
 export { default as Testimonials } from './Testimonials/Testimonials'
 export { default as Blog } from './Blog/Blog'
@@ -10,5 +11,6 @@ export { default as Experience } from'./Experience/Experience'
 export { default as Projects } from'./Projects/Projects'
 export { default as SingleProject } from './Projects/SingleProject/SingleProject'
 export { default as Achievement } from './Achievement/Achievement'
+*/
 
 export { default as BackToTop } from'./BackToTop/BackToTop'
