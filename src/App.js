7210b17ddeab101809f6c0f42fc50599fd7c44d8
Priyanshu Sinha <Priyanshu_Sinha@outlook.com>
2022-08-31 01:52:00 -0400

    - Minor fixes. Fixed build error.
    Signed-off-by: Priyanshu Sinha <Priyanshu_Sinha@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import { Main, BlogPage, ProjectPage } from './pages'
+import { Main} from './pages'
 import { BackToTop } from './components'
 import ScrollToTop from './utils/ScrollToTop'
 
@@ -14,9 +14,6 @@ function App() {
         <ScrollToTop/>
         <Switch>
           <Route path="/" exact component={Main} />
-          <Route path="/blog" exact component={BlogPage} />
-          <Route path="/projects" exact component={ProjectPage} />
-
           <Redirect to="/" />
         </Switch>
       </Router>
